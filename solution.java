5e1c4a11a202f067b94afb75108f01f93711e3fc
Aroni68 <31354559+Aroni68@users.noreply.github.com>
2017-10-18 19:34:42 +0600

    solution commited

---
+++ solution.java
@@ -0,0 +1,67 @@
+package hello;
+
+public class solution {
+
+
+			double addition()
+			{
+				    double a;
+					double b;
+					double c;
+				 Scanner math1 = new Scanner(System.in);
+				 System.out.println("First Num");
+				 a = math1.nextInt();
+				 System.out.print("Second Num");
+		         b = math1.nextInt();
+			     c = a + b;
+			     System.out.println ("The total value is " + c);
+				 return;
+
+			}
+			double substraction()
+			{
+				 double a;
+					double b;
+					double c;
+				Scanner math1=new Scanner(System.in);
+				System.out.println("First Num");
+				a = math1.nextInt();
+				System.out.print("Second Num");
+		        b = math1.nextInt();
+			     c = a - b;
+			     System.out.println ("The total value is " + c);
+				 return;
+
+			}
+			double multiply()
+			{
+				 double a;
+					double b;
+					double c;
+				Scanner math1=new Scanner(System.in);
+				System.out.println("First Num");
+				a = math1.nextInt();
+				System.out.print("Second Num");
+		        b = math1.nextInt();
+			     c = a * b;
+			     System.out.println ("The total value is " + c);
+				 return;
+
+			}
+
+			double division()
+			{
+				 double a;
+					double b;
+					double c;
+				Scanner math1=new Scanner(System.in);
+				System.out.println("First Num");
+				a = math1.nextInt();
+				System.out.print("Second Num");
+		        b = math1.nextInt();
+			     c = a / b;
+			     System.out.println ("The total value is " + c);
+				 return;
+
+			}
+}
